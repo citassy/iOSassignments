9291c4eb896673291619a9e3c8ae951db807c412
Mai Vu <vumaiphu@grinnells-macbook-air.local>
2018-11-16 16:36:56 -0600

    Initial Commit

---
+++ Toy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toy2
+//
+//  Created by Mai Phuong Vu on 11/16/18.
+//  Copyright © 2018 Citassy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
